dcb169d9aff46180ecc56e5fae51af353308d2a9
Uriel Solis <euss99@hotmail.com>
2025-06-08 12:27:33 -0600

    🔧 config: update eslint and prettier configuration

---
+++ eslint.config.js
@@ -0,0 +1,52 @@
+import eslint from '@eslint/js';
+import tseslint from '@typescript-eslint/eslint-plugin';
+import tsparser from '@typescript-eslint/parser';
+import prettier from 'eslint-plugin-prettier';
+import globals from 'globals';
+
+export default [
+  {
+    ignores: ['node_modules/**', 'dist/**', 'coverage/**', '.git/**'],
+  },
+  eslint.configs.recommended,
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      parser: tsparser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
+      globals: {
+        ...globals.node,
+        ...globals.es2021,
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tseslint,
+      prettier: prettier,
+    },
+    rules: {
+      '@typescript-eslint/explicit-function-return-type': 'warn',
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+      'prettier/prettier': ['error', { tabWidth: 2, singleQuote: false }],
+      'no-console': ['warn', { allow: ['warn', 'error'] }],
+      'no-duplicate-imports': 'error',
+      'no-unused-expressions': 'error',
+      'no-unused-vars': 'off',
+      indent: ['error', 2],
+      quotes: ['error', 'double'],
+      'sort-imports': [
+        'error',
+        {
+          ignoreCase: false,
+          ignoreDeclarationSort: true,
+          ignoreMemberSort: false,
+          memberSyntaxSortOrder: ['none', 'all', 'multiple', 'single'],
+          allowSeparatedGroups: true,
+        },
+      ],
+    },
+  },
+];
