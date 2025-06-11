b066f06b916188db19d0b14edaa50752894b18e9
Uriel Solis Salinas <euss99@hotmail.com>
2025-06-11 17:38:55 -0600

    ✨ feat: add morgan logging middleware and update package dependencies in package.json and package-lock.json

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import colors from "colors";
 import cors from "cors";
 import express from "express";
+import morgan from "morgan";
 import swaggerUi from "swagger-ui-express";
 
 import { corsOptions } from "@/config/cors.config";
@@ -17,6 +18,7 @@ const port = envConfig.PORT;
 // Middleware
 app.use(cors(corsOptions));
 app.use(express.json());
+app.use(morgan("dev"));
 
 // Swagger documentation
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
